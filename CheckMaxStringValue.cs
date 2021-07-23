c449f2b67e72e10aa5b6f69ba3e80df99dd999d7
Neha121995 <jagtapneha1212@gmail.com>
2021-07-23 21:44:08 +0530

    [Neha] Added: Take 3 Strings find Maximum Value

---
+++ CheckMaxStringValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Problem_C_Sharp
+{
+    class CheckMaxStringValue
+    {
+        public static string MaxStringNumber(string fString, string sString, string tString)
+        {
+            if (fString.CompareTo(sString) > 0 && fString.CompareTo(tString) > 0 ||
+                fString.CompareTo(sString) >= 0 && fString.CompareTo(tString) > 0 ||
+                fString.CompareTo(sString) > 0 && fString.CompareTo(tString) >= 0)
+            {
+                return fString;
+            }
+
+            if (sString.CompareTo(fString) > 0 && sString.CompareTo(tString) > 0 ||
+                sString.CompareTo(fString) >= 0 && sString.CompareTo(tString) > 0 ||
+                sString.CompareTo(fString) > 0 && sString.CompareTo(tString) >= 0)
+            {
+                return sString;
+            }
+
+            if (tString.CompareTo(fString) > 0 && tString.CompareTo(sString) > 0 ||
+                tString.CompareTo(fString) >= 0 && tString.CompareTo(sString) > 0 ||
+                tString.CompareTo(fString) > 0 && tString.CompareTo(sString) >= 0)
+            {
+                return tString;
+            }
+
+            throw new Exception("FirstStringValue,SecondStringValue and ThirdStringValue Are Same");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace Generics_Problem_C_Sharp
         static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum Float Value");
-            float floatMaxValue = CheckMaxFloatValue.MaxFloatValue(43.5f, 98.4f, 10.23f);
-            Console.WriteLine("Maximum Float Value is :: " + floatMaxValue);
+            string maxStringValue = CheckMaxStringValue.MaxStringNumber("Neha", "Nikita", "Karan");
+            Console.WriteLine("Maximum String Value is :: " + maxStringValue);
         }
     }
 }
