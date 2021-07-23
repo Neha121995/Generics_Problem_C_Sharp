88f51dddd09632f5ca4051261a187f5c2d0bcde0
Neha121995 <jagtapneha1212@gmail.com>
2021-07-24 00:06:22 +0530

    [Neha] Added: Max Method More than three parameters

---
+++ Generics_UsingMaxCount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Problem_C_Sharp
+{
+    class Generics_UsingMaxCount<T> where T : IComparable<T>
+    {
+        List<T> maxValue = new List<T>();
+        public Generics_UsingMaxCount(params T[] values)
+        {
+            this.ValuesBuilder(values);
+        }
+        public void ValuesBuilder(T[] maxValue)
+        {
+            foreach (T element in maxValue)
+            {
+                this.maxValue.Add(element);
+            }
+        }
+        public void threeIFSMaxValues()
+        {
+            this.maxValue.Sort();
+            Console.WriteLine(this.maxValue[this.maxValue.Count - 1]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace Generics_Problem_C_Sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find Maximum Float Value");
-            string maxStringValue = CheckMaxStringValue.MaxStringNumber("Neha", "Nikita", "Karan");
-            Console.WriteLine("Maximum String Value is :: " + maxStringValue);
+            Generics_UsingMaxCount<int> intValue = new Generics_UsingMaxCount<int>(76, 54, 87, 12, 78, 20, 89, 13);
+            intValue.threeIFSMaxValues();
+            Generics_UsingMaxCount<float> floatValue = new Generics_UsingMaxCount<float>(56.8f, 12.0f, 30.34f, 23.90f, 67.23f, 34.89f, 34.90f, 108.90f, 20.30f);
+            floatValue.threeIFSMaxValues();
+            Generics_UsingMaxCount<string> stringValue = new Generics_UsingMaxCount<string>("Neha", "Karan", "Ruturaj", "Sandip", "Shivani");
+            stringValue.threeIFSMaxValues();
         }
     }
 }
